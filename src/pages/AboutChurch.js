7ea70ad3923e3da520317ac29c41572225a8fa6c
Daria Korsun <136754269+dariakorsunart@users.noreply.github.com>
2025-09-12 12:46:23 -0400

    Update AboutChurch.js

--- src/pages/AboutChurch.js
+++ src/pages/AboutChurch.js
@@ -9,14 +9,13 @@ function AboutChurch() {
         <h1>{t('about_church.title')}</h1>
         <h2>{t('about_church.greetings')}</h2>
         
-        {/* Новый контейнер для текста и изображения */}
         <div className="church-content-grid">
           {/* Колонка с текстом слева */}
           <div className="church-text-column">
             <p>{t('about_church.description')}</p>
           </div>
           
-          {/* Колонка с изображением справа */}
+          
           <div className="church-image-column">
             <img src={process.env.PUBLIC_URL +"/images/saintTikhon.jpg"} alt="St. Patriarch Tikhon" className="church-image" />
           </div>
