fc5c33358d242bbdc8334e8a62cee5bdffb24591
Daria Korsun <136754269+dariakorsunart@users.noreply.github.com>
2025-09-12 12:41:42 -0400

    Update SchoolNutrition.js

--- src/pages/SchoolNutrition.js
+++ src/pages/SchoolNutrition.js
@@ -3,13 +3,11 @@ import { useTranslation } from 'react-i18next';
 import { Viewer } from '@react-pdf-viewer/core';
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 
-// Import styles
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 function SchoolNutrition() {
   const { t } = useTranslation();
 
- // Путь к вашему PDF-файлу
   const pdfUrl = process.env.PUBLIC_URL +'/School_Nutrition.pdf';
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
