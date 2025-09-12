9ab1de2f34c08198f718b113686f7a2ac15ec498
Daria Korsun <136754269+dariakorsunart@users.noreply.github.com>
2025-09-12 12:38:52 -0400

    Update SchoolCharter.js

--- src/pages/SchoolCharter.js
+++ src/pages/SchoolCharter.js
@@ -3,14 +3,14 @@ import { useTranslation } from 'react-i18next';
 import { Viewer } from '@react-pdf-viewer/core';
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 
-// Import styles
+
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
 function SchoolCharter() {
   const { t } = useTranslation();
 
-  // Путь к вашему PDF-файлу
+ 
   const pdfUrl = process.env.PUBLIC_URL +'/school_charter.pdf';
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
